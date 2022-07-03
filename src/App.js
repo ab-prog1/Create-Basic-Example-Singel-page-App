b09c58d7f4ad73dd4fdfed77ad9aef47eea54bb2
Abolfazl Ghorbani <ab.ghorbani28@gmail.com>
2022-07-03 12:15:04 +0200

    Create-Basic-Example-Singel-page-App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-// import RouterUser from "./pages/users/Router";
+import RouterUser from "./pages/users/Router";
 // import RouterPost from "./pages/posts/Router";
 
 
@@ -11,8 +11,8 @@ function App() {
       <Header />
       <Routes>
         <Route  path="/" element={<Home/>} />
-        {/* <Route path="/users/*" element={<RouterUser/>} />
-        <Route path="/posts/*" element={<RouterPost/>} /> */}
+        <Route path="/users/*" element={<RouterUser/>} />
+        {/* <Route path="/posts/*" element={<RouterPost/>} /> */}
       </Routes>
     </BrowserRouter>
   );
